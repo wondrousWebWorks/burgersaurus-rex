b2d059fab16e5625cd440e5e1149ac495ad5c178
Johann Alberts <alberts.johann.com>
2020-06-26 07:17:40 +0100

    Finished fixtures for menu, loaded data, set up basic menu app with a path and basic view function

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Allergen, Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'is_vegetarian',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+class AllergenAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Allergen, AllergenAdmin)
+admin.site.register(Product, ProductAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -11,6 +15,21 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Menu(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+class Allergen(models.Model):
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    is_vegetarian = models.BooleanField(null=True, blank=True)
+    allergens = models.ManyToManyField('Allergen')
+
+    def __str__(self):
+        return self.name
