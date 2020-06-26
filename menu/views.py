56a5ae2a660b9351e7e96424d0bc5f0ace8fcdfa
Johann Alberts <alberts.johann.com>
2020-06-26 09:05:33 +0100

    Added menu filtering by category

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from .models import Product, Category
+
 def menu(request):
 
+    menu = Product.objects.all()
+    category = None
 
-    return render(request, 'menu/menu.html')
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            menu = menu.filter(category__name__in=categories)
+
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'menu/menu.html', context)
