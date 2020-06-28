612426b88ddb808747ae4944c6e51a787a6fdd07
Johann Alberts <alberts.johann.com>
2020-06-28 16:51:41 +0100

    Finished the base menu page and add links to filter categories

--- menu/views.py
+++ menu/views.py
@@ -5,15 +5,17 @@ from .models import Product, Category
 def menu(request):
 
     menu = Product.objects.all()
-    category = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             menu = menu.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'menu': menu,
+        'categories': categories
     }
 
     return render(request, 'menu/menu.html', context)
