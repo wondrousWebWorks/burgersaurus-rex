65ad2624d964ffdd705a5cb1820b9ab0518a72f7
Johann Alberts <alberts.johann.com>
2020-07-07 02:11:45 +0100

    Added function to update the order total on deletion of an order item

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,6 +6,14 @@ from .models import OrderItem
 @receiver(post_save, sender=OrderItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
-    Update order total on lineitem update/create
+    Update the order total on orderitem create/update
     """
     instance.order.update_order_total()
+
+
+@receiver(post_delete, sender=OrderItem)
+def update_on_delete(sender, instance, **kwargs):
+    """
+    Update the order total on deletion of orderitem
+    """
+    instance.order.update_total()
