ee1818d61190cff3297646a36210e00c2a19dc40
Johann Alberts <alberts.johann.com>
2020-07-07 15:06:56 +0100

    Added templatetags to calculate the item subtotal on cart and checkout pages and create toasts for messages

---
+++ cart/templatetags/calculate_item_subtotal.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='calculate_item_subtotal')
+def calculate_item_subtotal(price, quantity):
+    return price * quantity
