cf22d5f2dbe01eae972e9942b766fe75d4b8d699
Johann Alberts <alberts.johann.com>
2020-07-07 01:59:49 +0100

    Registered Order models and added Admin classes for Orders to customise the dashboard display

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('order_item_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_sub_total',
+                       'order_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'postcode', 
+              'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_sub_total', 'order_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_sub_total', 'delivery_cost',
+                    'order_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
