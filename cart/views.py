8563bf50caa83fa6ade534793756416974019790
Johann Alberts <alberts.johann.com>
2020-07-08 18:15:07 +0100

    Updated menu and add_to_cart view so that the form data is sent via a fetch request instead of handling via django only. Also changed a few button classes

--- cart/views.py
+++ cart/views.py
@@ -13,19 +13,23 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a specified quantity of a given product to the shopping cart """
 
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart', {})
+    try:
+        request_body = json.loads(request.body)
+        quantity = int(request_body['quantity'])
+        cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-    else:
-        cart[item_id] = quantity
-        messages.success(request, 'Successfully added product to cart!')
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+            messages.success(request, 'Successfully added product to cart!')
 
-    request.session['cart'] = cart
+        request.session['cart'] = cart
 
-    return redirect(redirect_url)
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
 
 
 def update_cart(request, item_id):
