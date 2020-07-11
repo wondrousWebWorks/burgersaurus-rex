111c9f0e8fe278cdbeea8dc0a451283577250acd
Johann Alberts <alberts.johann.com>
2020-07-11 23:38:58 +0100

    Created stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -8,6 +8,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'Your cart is empty!')
@@ -16,13 +19,18 @@ def checkout(request):
     current_cart = cart_content(request)
     cart_total = current_cart['order_total']
     stripe_total = round(cart_total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency = settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51GqPsYKP14WGiq4LBCGkpzDps5JORfc9euGYZUeYnUBmby6Uzqcg0KMqQTuNDFw1dOOXHG3h4XH0QBoAeACTd46V00lnRRF4t9',
-        'client_secret': 'test-client-secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
