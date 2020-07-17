21cdda9780e88be5d090df29746cdbaf49f1d412
Johann Alberts <alberts.johann@gmail.com>
2020-07-17 20:41:29 +0100

    Updated profile view and template, added UserProfile form and ajusted form styling

---
+++ profiles/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import UserProfile
+
+CITIES = [
+    ('', 'Select a City'),
+    ('Cork', 'Cork'),
+    ('Dublin', 'Dublin'),
+    ('Galway', 'Galway'),
+    ('Limerick', 'Limerick'),
+    ('Waterford', 'Waterford')
+]
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders, set autofocus on first field and remove labels 
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_town_or_city': 'City',
+            'default_postcode': 'Postal Code',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['default_town_or_city'] = forms.ChoiceField(choices=CITIES)
+        for field in self.fields:
+            if field != 'default_town_or_city':
+                if self.fields[field].required:
+                    placeholder = f'* {placeholders[field]}'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib import messages
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     user_profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated profile!')
+
+    form = UserProfileForm(instance=user_profile)
+    orders = user_profile.orders.all()
     template = 'profiles/profile.html'
     context = {
-        'profile': user_profile
+        'form': form,
+        'orders': orders
     }
 
     return render(request, template, context)
