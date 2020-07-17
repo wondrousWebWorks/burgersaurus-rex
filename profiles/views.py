16595ffc586085ccc48d459d0bb74e6ffa4ab205
Johann Alberts <alberts.johann.com>
2020-07-17 19:19:34 +0100

    Updated navbar link to show register instead of sign up, added some styling to allauth templates and tested registering functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import UserProfile
 
 
 def profile(request):
     """ Display the user's profile. """
+    user_profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': user_profile
+    }
 
     return render(request, template, context)
