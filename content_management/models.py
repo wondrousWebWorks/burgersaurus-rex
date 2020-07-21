1288c621a1a501e583e6b2702bbe957bd2ec580f
Johann Alberts <alberts.johann@gmail.com>
2020-07-22 00:36:12 +0100

    Added dark_mode field to Theme model

--- content_management/models.py
+++ content_management/models.py
@@ -7,6 +7,7 @@ class Theme(models.Model):
     colour_3 = models.CharField(max_length=6, null=False, blank=False)
     colour_4 = models.CharField(max_length=6, null=False, blank=False)
     colour_5 = models.CharField(max_length=6, null=False, blank=False)
+    dark_mode_background_colour = models.CharField(max_length=6, null=False, blank=False, default='1c1c1c')
 
     def __str__(self):
         return self.name
