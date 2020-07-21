b49c3151776c3c6a0dc635a4cd1e46784ec0563c
Johann Alberts <alberts.johann@gmail.com>
2020-07-22 00:23:33 +0100

    Created Theme model and registered it

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,3 +1,16 @@
+from content_management.models import Theme
 from django.contrib import admin
 
-# Register your models here.
+from .models import Theme
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'colour_1',
+        'colour_2',
+        'colour_3',
+        'colour_4',
+        'colour_5',
+    )
+
+admin.site.register(Theme, ThemeAdmin)
--- content_management/models.py
+++ content_management/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class Theme(models.Model):
+    name = models.CharField(max_length=254, null=True, blank=True)
+    colour_1 = models.CharField(max_length=6, null=False, blank=False)
+    colour_2 = models.CharField(max_length=6, null=False, blank=False)
+    colour_3 = models.CharField(max_length=6, null=False, blank=False)
+    colour_4 = models.CharField(max_length=6, null=False, blank=False)
+    colour_5 = models.CharField(max_length=6, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
