d61f74c9731b7ef756ba434c97f31814236ef500
Johann Alberts <alberts.johann@gmail.com>
2020-07-22 00:00:47 +0100

    Got theme chaning working. JUST A TEST! Need te retieve data from Model

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,11 @@
 /*jshint esversion: 6 */
 
+const themesWrapper = document.querySelector('#theme-options');
+const themeSelectors = document.querySelectorAll('.theme-option');
+
+/**
+ * Toggles the navbar animation when scrolling past a certain screen height
+ */
 const toggleNavbarBackground = () => {
     const nav = document.querySelector('#navbar');
     const deliveryBanner = document.querySelector('#delivery-banner');
@@ -9,14 +15,53 @@ const toggleNavbarBackground = () => {
 
 window.addEventListener('scroll', toggleNavbarBackground);
 
-const themesWrapper = document.querySelector('#theme-options');
+
 document.querySelector('#theme-selector-trigger').addEventListener('click', () => {
     themesWrapper.classList.toggle('translate-left');
 });
 
-// document.documentElement.style.setProperty('--colour-1', '7, 59, 76');
-// document.documentElement.style.setProperty('--colour-2', '6, 214, 160');
-// document.documentElement.style.setProperty('--colour-3', '239, 71, 111');
-// document.documentElement.style.setProperty('--colour-4', '255, 209, 102');
-// document.documentElement.style.setProperty('--colour-5', '17, 138, 178');
+themeSelectors.forEach(themeSelector => {
+    themeSelector.addEventListener('click', () => {
+        const themeSelectorClassList = themeSelector.classList;
+
+        if (themeSelectorClassList.contains('theme-1')) {
+            sessionStorage.setItem("theme", "1");
+            document.documentElement.style.setProperty('--colour-1', '6, 214, 160');
+            document.documentElement.style.setProperty('--colour-2', '239, 71, 111');
+            document.documentElement.style.setProperty('--colour-3', '255, 209, 102');
+            document.documentElement.style.setProperty('--colour-4', '17, 138, 178');
+            document.documentElement.style.setProperty('--colour-5', '7, 59, 76');
+        } else if (themeSelectorClassList.contains('theme-2')) {
+            sessionStorage.setItem("theme", "2");
+            document.documentElement.style.setProperty('--colour-2', '6, 214, 160');
+            document.documentElement.style.setProperty('--colour-3', '239, 71, 111');
+            document.documentElement.style.setProperty('--colour-4', '255, 209, 102');
+            document.documentElement.style.setProperty('--colour-5', '17, 138, 178');
+            document.documentElement.style.setProperty('--colour-1', '7, 59, 76');
+        } else if (themeSelectorClassList.contains('theme-3')) {
+            sessionStorage.setItem("theme", "3");
+            document.documentElement.style.setProperty('--colour-3', '6, 214, 160');
+            document.documentElement.style.setProperty('--colour-4', '239, 71, 111');
+            document.documentElement.style.setProperty('--colour-5', '255, 209, 102');
+            document.documentElement.style.setProperty('--colour-1', '17, 138, 178');
+            document.documentElement.style.setProperty('--colour-2', '7, 59, 76');
+        } else if (themeSelectorClassList.contains('theme-4')) {
+            sessionStorage.setItem("theme", "4");
+            document.documentElement.style.setProperty('--colour-4', '6, 214, 160');
+            document.documentElement.style.setProperty('--colour-5', '239, 71, 111');
+            document.documentElement.style.setProperty('--colour-1', '255, 209, 102');
+            document.documentElement.style.setProperty('--colour-2', '17, 138, 178');
+            document.documentElement.style.setProperty('--colour-3', '7, 59, 76');
+        } else if (themeSelectorClassList.contains('dark-mode')) {
+            document.documentElement.style.setProperty('--body-colour', '10, 10, 10');
+        }
+    });
+});
+
+
+// document.documentElement.style.setProperty('--colour-1', '6, 214, 160');
+// document.documentElement.style.setProperty('--colour-2', '239, 71, 111');
+// document.documentElement.style.setProperty('--colour-3', '255, 209, 102');
+// document.documentElement.style.setProperty('--colour-4', '17, 138, 178');
+// document.documentElement.style.setProperty('--colour-5', '7, 59, 76');
 // document.documentElement.style.setProperty('--body-colour', '10, 10, 10');
