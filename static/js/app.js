220527cfc1abf7ca6f3b0c8baf24d452e6eb4e9e
Johann Alberts <alberts.johann@gmail.com>
2020-07-25 19:10:23 +0100

    Added functionality to let themes persist between apps

--- static/js/app.js
+++ static/js/app.js
@@ -10,6 +10,9 @@ let colour3 = '255, 209, 102';
 let colour4 = '17, 138, 178';
 let colour5 = '7, 59, 76';
 let darkModeColor = '28, 28, 28';
+let lightModeColor = '247, 247, 247';
+let theme = "1";
+let darkMode = 'false';
 
 /**
  * Toggles the navbar animation when scrolling past a certain screen height
@@ -33,36 +36,104 @@ themeSelectors.forEach(themeSelector => {
         const themeSelectorClassList = themeSelector.classList;
 
         if (themeSelectorClassList.contains('theme-1')) {
-            sessionStorage.setItem("theme", "1");
-            document.documentElement.style.setProperty('--colour-1', '6, 214, 160');
-            document.documentElement.style.setProperty('--colour-2', '239, 71, 111');
-            document.documentElement.style.setProperty('--colour-3', '255, 209, 102');
-            document.documentElement.style.setProperty('--colour-4', '17, 138, 178');
-            document.documentElement.style.setProperty('--colour-5', '7, 59, 76');
+            sessionStorage.setItem('theme', '1');
+            setCSSColour('--colour-1', colour1);
+            setCSSColour('--colour-2', colour2);
+            setCSSColour('--colour-3', colour3);
+            setCSSColour('--colour-4', colour4);
+            setCSSColour('--colour-5', colour5);
         } else if (themeSelectorClassList.contains('theme-2')) {
-            sessionStorage.setItem("theme", "2");
-            document.documentElement.style.setProperty('--colour-2', '6, 214, 160');
-            document.documentElement.style.setProperty('--colour-3', '239, 71, 111');
-            document.documentElement.style.setProperty('--colour-4', '255, 209, 102');
-            document.documentElement.style.setProperty('--colour-5', '17, 138, 178');
-            document.documentElement.style.setProperty('--colour-1', '7, 59, 76');
+            sessionStorage.setItem("theme", '2');
+            setCSSColour('--colour-1', colour2);
+            setCSSColour('--colour-2', colour3);
+            setCSSColour('--colour-3', colour4);
+            setCSSColour('--colour-4', colour5);
+            setCSSColour('--colour-5', colour1);
         } else if (themeSelectorClassList.contains('theme-3')) {
-            sessionStorage.setItem("theme", "3");
-            document.documentElement.style.setProperty('--colour-3', '6, 214, 160');
-            document.documentElement.style.setProperty('--colour-4', '239, 71, 111');
-            document.documentElement.style.setProperty('--colour-5', '255, 209, 102');
-            document.documentElement.style.setProperty('--colour-1', '17, 138, 178');
-            document.documentElement.style.setProperty('--colour-2', '7, 59, 76');
+            sessionStorage.setItem('theme', '3');
+            setCSSColour('--colour-1', colour3);
+            setCSSColour('--colour-2', colour4);
+            setCSSColour('--colour-3', colour5);
+            setCSSColour('--colour-4', colour1);
+            setCSSColour('--colour-5', colour2);
         } else if (themeSelectorClassList.contains('theme-4')) {
-            sessionStorage.setItem("theme", "4");
-            document.documentElement.style.setProperty('--colour-4', '6, 214, 160');
-            document.documentElement.style.setProperty('--colour-5', '239, 71, 111');
-            document.documentElement.style.setProperty('--colour-1', '255, 209, 102');
-            document.documentElement.style.setProperty('--colour-2', '17, 138, 178');
-            document.documentElement.style.setProperty('--colour-3', '7, 59, 76');
+            sessionStorage.setItem('theme', '4');
+            setCSSColour('--colour-1', colour4);
+            setCSSColour('--colour-2', colour5);
+            setCSSColour('--colour-3', colour1);
+            setCSSColour('--colour-4', colour2);
+            setCSSColour('--colour-5', colour3);
         } else if (themeSelectorClassList.contains('dark-mode')) {
-            sessionStorage.setItem("dark-mode", "true");
-            document.documentElement.style.setProperty('--body-colour', darkModeColor);
+            let darkModeCheck = sessionStorage.getItem('dark-mode');
+            
+            if (darkModeCheck === 'false') {
+                sessionStorage.setItem('dark-mode', 'true');
+                setCSSColour('--body-colour', darkModeColor);
+                document.querySelector('.dark-mode').style.backgroundColor = '#f7f7f7';
+            } else {
+                sessionStorage.setItem('dark-mode', 'false');
+                setCSSColour('--body-colour', lightModeColor);
+                document.querySelector('.dark-mode').style.backgroundColor = '#1c1c1c';
+            }
         }
     });
+});
+
+const setCSSColour = (target, colour) => {
+    document.documentElement.style.setProperty(target, colour);
+};
+
+const getTheme = () => {
+    theme = sessionStorage.getItem('theme');
+    darkMode = sessionStorage.getItem('dark-mode');
+};
+
+const setTheme = () => {
+    switch(theme) {
+        case '1':
+            setCSSColour('--colour-1', colour1);
+            setCSSColour('--colour-2', colour2);
+            setCSSColour('--colour-3', colour3);
+            setCSSColour('--colour-4', colour4);
+            setCSSColour('--colour-5', colour5);
+            break;
+        case '2':
+            setCSSColour('--colour-1', colour2);
+            setCSSColour('--colour-2', colour3);
+            setCSSColour('--colour-3', colour4);
+            setCSSColour('--colour-4', colour5);
+            setCSSColour('--colour-5', colour1);
+            break;
+        case '3':
+            setCSSColour('--colour-1', colour3);
+            setCSSColour('--colour-2', colour4);
+            setCSSColour('--colour-3', colour5);
+            setCSSColour('--colour-4', colour1);
+            setCSSColour('--colour-5', colour2);
+            break;
+        case '4':
+            setCSSColour('--colour-1', colour4);
+            setCSSColour('--colour-2', colour5);
+            setCSSColour('--colour-3', colour1);
+            setCSSColour('--colour-4', colour2);
+            setCSSColour('--colour-5', colour3);
+            break;
+        default:
+            setCSSColour('--colour-1', colour1);
+            setCSSColour('--colour-2', colour2);
+            setCSSColour('--colour-3', colour3);
+            setCSSColour('--colour-4', colour4);
+            setCSSColour('--colour-5', colour5);
+    }
+
+    if (darkMode === 'true') {
+        setCSSColour('--body-colour', darkModeColor);
+    } else {
+        setCSSColour('--body-colour', lightModeColor);
+    }
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+    getTheme();
+    setTheme();
 });
