9cd0a9d966c37325efce79afd90bd9f944caf224
Johann Alberts <alberts.johann@gmail.com>
2020-07-25 19:35:14 +0100

    Adapted about app to use images form the Image model

--- about/views.py
+++ about/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+from images.models import Image, Page
+
 def about(request):
     """Returns a rendered template of the about page"""
+    images = Image.objects.all()
 
-    return render(request, 'about/about.html')
+    about_header = images.filter(image_name__icontains='about-header')[0]
+    about_farm = images.filter(image_name__icontains='about-farm')[0]
+    about_hydroponics = images.filter(image_name__icontains='about-hydroponics')[0]
+
+    template = 'about/about.html'
+
+    context = {
+        'about_header': about_header,
+        'about_farm': about_farm,
+        'about_hydroponics': about_hydroponics,
+    }
+
+    return render(request, template, context)
