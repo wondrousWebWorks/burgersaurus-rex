3dc13e43de194a9aa523e79b60216f27f59d4bc7
Johann Alberts <alberts.johann@gmail.com>
2020-07-26 11:51:47 +0100

    Added image_friendly_name field to Image model

--- images/models.py
+++ images/models.py
@@ -9,5 +9,6 @@ class Page(models.Model):
 
 class Image(models.Model):
     page = models.ForeignKey('Page', null=True, blank=True, on_delete=models.SET_NULL)
-    image_name = models.CharField(max_length=254, null=True, blank=True)
+    image_name = models.CharField(max_length=254, null=False, blank=False, default='default')
+    image_friendly_name = models.CharField(max_length=254, null=False, blank=False, default='Default')
     image = models.ImageField(upload_to='images/', null=True, blank=False)
