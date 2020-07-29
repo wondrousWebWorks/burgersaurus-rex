656407f765a1fa37aefa79bce951ae2b3fffeb79
Johann Alberts <alberts.johann@gmail.com>
2020-07-29 23:05:02 +0100

    Set secret key in heroku and removed from settings.py. Also set debug based on environment variable

--- burgersaurus/settings.py
+++ burgersaurus/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3aajh0lupzsthh!^irkdatwtwg&5rwayi!04ltqx!4*6)_9jz@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['burgersaurus-rex', 'localhost']
 
